b94c63212bedc692a842ef1315a1c982858dc011
yudaapratama <yudhaprathamaa@gmail.com>
2025-06-01 15:12:25 +0700

    feat: layouting login page in admin

--- admin/src/components/icon/index.js
+++ admin/src/components/icon/index.js
@@ -5,3 +5,4 @@ export { default as weibo } from './weibo.svg?react';
 export { default as qq } from './qq.svg?react';
 export { default as google } from './google.svg?react';
 export { default as shinigami } from './shinigami.svg?react';
+export { default as discord } from './discord.svg?react';
